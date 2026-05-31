e69bc204a61687c4c5ac8dbb4b6214a4ab831400
Gogglehotmail <106886063+Gogglehotmail@users.noreply.github.com>
2026-05-31 15:01:28 +0200

    run.sh aktualisieren

--- hermes/run.sh
+++ hermes/run.sh
@@ -1,8 +1,2 @@
-#!/usr/bin/with-contenv bashio
-
-echo "Starting Hermes ...."
-
-export HASS_URL=$(bashio::config 'hass_url')
-export HASS_TOKEN=$(bashio::config 'hass_token')
-
-exec python3 /server.py
+#!/bin/bash
+exec python /server.py
