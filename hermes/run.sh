97fc257c252197c73efcbf8ccb0ad4d5e1b78889
Gogglehotmail <106886063+Gogglehotmail@users.noreply.github.com>
2026-05-31 14:35:39 +0200

    run.sh erstellen

---
+++ hermes/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Starting Hermes Add-on..."
+
+export HASS_URL=$(bashio::config 'hass_url')
+export HASS_TOKEN=$(bashio::config 'hass_token')
+
+exec python /server.py
